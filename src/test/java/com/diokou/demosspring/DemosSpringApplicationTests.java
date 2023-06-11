aed84edc54155f2832efb5751651505ea5f26124
diokou-tech <diokoucheikh@gmail.com>
2023-06-11 11:48:20 +0000

    First commit

---
+++ src/test/java/com/diokou/demosspring/DemosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diokou.demosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
